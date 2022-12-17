bd044ac10f92d717c6b6bbf4b455c19728eddfcf
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-12-17 13:27:24 +0300

    Homeworks 2 Task 4 updated

--- Homeworks/Homework2/Task4.py
+++ Homeworks/Homework2/Task4.py
@@ -11,6 +11,19 @@ with open('file.txt', 'w') as data:
     data.write('0\n')
     data.write('3\n')
     data.write('4')
-data.close()
 
+path = 'file.txt'
+with open(path, 'r') as data:
+    num1 = data.readline()
+    print(int(num1))
+    num2 = data.readline()
+    print(int(num2))
+    num3 = data.readline()
+    print(int(num3))
 
+i1 = int(num1)
+i2 = int(num2)
+i3 = int(num3)
+
+result = list_num[i1]*list_num[i2]*list_num[i3]
+print(result)

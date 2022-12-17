0b7f80e3066dd8cc612c3b6bfe514efd38ff822c
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-12-17 11:03:21 +0300

    Homework 2 Task 3 updated

--- Homeworks/Homework2/Task3.py
+++ Homeworks/Homework2/Task3.py
@@ -5,5 +5,5 @@
 
 
 n = int(input('Введите число: '))
-lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
+lst = [round((1+1/i)**i, 2) for i in range(1, n+1)]
 print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 2)}')
